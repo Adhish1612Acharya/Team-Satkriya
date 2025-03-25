928efa8b5dcd5a401126c83b6505baa5644ecd8f
adhisha0505 <adhisha0505@gmail.com>
2025-03-25 17:28:27 +0000

    Added filters badge to workshops

--- src/pages/WorkShops/WorkShops.tsx
+++ src/pages/WorkShops/WorkShops.tsx
@@ -71,7 +71,7 @@ const WorkshopsPage = () => {
             ))}
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6">
             {workshops.map((workshop) => (
               <WorkshopCard key={workshop.id} workshop={workshop} />
             ))}
